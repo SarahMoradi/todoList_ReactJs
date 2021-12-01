7d7a7e01369cd6a8ef8c141abcade057d4663fd1
Moradi <s.moradi@asiatech.cloud>
2021-12-01 16:19:10 +0330

    Updated the delete button

--- src/component/ShowTask.js
+++ src/component/ShowTask.js
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BsJournalPlus } from 'react-icons/bs';
 import styles from './index.module.css';
 import { Input, Button, Card, Row, Col, Container, CardTitle, CardBody, CardSubtitle, CardText } from 'reactstrap';
 
 const ShowTask = props => {
+
+    const [todos, setTodos] = useState([]);
+
+    useEffect(() => {
+        setTodos(props.todoFromParent)
+    },[props])
+
+
+
+    const deleteHandler = (todo) => {
+        
+        let result = todos.filter(_todo =>_todo!=todo);
+
+        setTodos(result);
+
+    }
+    console.log(todos,props.todoFromParent);
+
     return(
         <Col
         className={`${styles.cardBackground} bg-light border p-4`}
@@ -12,15 +30,16 @@ const ShowTask = props => {
         md="6">
             
           <h4 className="text-center pb-2">Current Task</h4>
-          {props.todoFromParent.length>0 ? 
-          props.todoFromParent.map(todo=>
-          <Card>
+          {todos.length>0 ? 
+          todos.map(todo=>
+            
+          <Card key={todo}>
               <CardBody>
                   <Row 
                   className={`${styles.listItems}`}>
                     <Col sm='12' className="pb-3">
                         <p>
-{todo}
+                            {todo}
                         </p>
                     </Col>
                     <Col sm='12' className="pb-1 d-flex justify-content-end">
@@ -28,6 +47,7 @@ const ShowTask = props => {
                         color={'success'}
                         className='mx-3'>Done</Button>
                         <Button
+                        onClick={()=> deleteHandler(todo)}
                         color={'danger'}
                         >Delete</Button>
                     </Col>
